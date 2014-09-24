1505753aafb504295772834e3941e79611bc90b1
Mengfei Li <ml4713@nyu.edu>
2014-09-24 15:27:20 -0400

    Assignment3

---
+++ ml4713/my_package/hello_world.py
@@ -0,0 +1,24 @@
+'''
+Created on Sep 24, 2014
+
+@author: ds-ga-1007
+
+def addition():
+    return 100 + 500
+
+
+def hello_world():
+    print "Hello World!"
+
+'''
+
+def hello_world():
+    print ("Hello World!")
+    
+#
+# Python hello world
+#
+if __name__ == "__main__":
+        import sys
+        hello_world()
+        
